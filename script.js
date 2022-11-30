9ab5fadf7371322b385975c86ef2b061238c6961
jmartinsss <josemartinscontato12@outlook.com>
2022-11-30 09:20:08 -0300

    ADICIONAR OS TIPOS DE ENTRADAS

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 //Todo inicio de sistema o mesmo deve pedir o saldo inicial da peça!
     let saldoInicial = prompt('Qual o saldo inicial?');
-    let result = 'Saldo Inicial:' + ' ' + saldoInicial;
-    console.log(result);
+    let resultado = "Saldo Inicial:" + " " + saldoInicial;
+    alert(resultado);
+//Definir o tipo de entrada + quantidade
+//tipo 1: compra (saldo inicial + quantidade)
+//tipo 2: venda (saldo inicial - quantidade)...
+    let tipoEntrada1 = prompt('Qual o tipo de entrada?')
+    let resultado1 = "Tipo de entrada:" + " " + tipoEntrada1;
+    alert(resultado1);
+//Definir as possíveis opções de tipos de entrada!
+    if (tipoEntrada1 = "compra") {
+        let tipoEntrada1 = prompt('Qual a quantidade?')
+    }
+
