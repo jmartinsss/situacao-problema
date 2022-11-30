bea8852ae32b8ee1548ebca56fc4b982c44e1e34
jmartinsss <josemartinscontato12@outlook.com>
2022-11-29 23:24:37 -0300

    REFAZER SCRIPT

--- script.js
+++ script.js
@@ -2,25 +2,3 @@
     let saldoInicial = prompt('Qual o saldo inicial?');
     let result = 'Saldo Inicial:' + ' ' + saldoInicial;
     console.log(result);
-
-//Proximo passo é o sistema perguntar qual o tipo de entrada:
-//tipo 1: compra
-//tipo 2: venda
-let tipoEntrada = prompt('Qual o tipo de entrada?');
-let quantidadeTipo1 = prompt('Qual a quantidade?');
-console.log(quantidadeTipo1);
-
-function tipoEntrada(); {
-    tipoEntrada = quantidadeTipo1 + result;
-}
-
-let tipoEntrada = 'compra'
-console.log(tipoEntrada);
-//chamar o fuction no código!
-tipoEntrada();
-
-let resultTipo = tipoEntrada;
-console.log(resultTipo);
-//Proximo passo é desenvolver as possibilidades:
-//tipo 1: saldo + quantidade de peças (Se saldo > quantidade = 'Saldo Atualizado')
-//tipo 2: saldo - quantidade de peças (Se saldo < quantidade = "Saldo Insuficiente")
