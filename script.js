80e14fa3dbf4e2a1a3b98f5f817c797be01b8951
jmartinsss <josemartinscontato12@outlook.com>
2022-12-02 00:08:45 -0300

    CORRIGIR BUG#24
    O código está com um bug na linha 24, quando apresenta o resultado da operação.
    Ao invés de apresentar o resultado do tipo2. mostra os resultados dos dois tipos!

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 //Todo inicio de sistema o mesmo deve pedir o saldo inicial da peça!
     let saldoInicial = prompt('Qual o saldo inicial?');
     let resultado = "Saldo Inicial:" + " " + saldoInicial;
-    alert(resultado);
+    console.log(resultado);
 //Definir o tipo de entrada + quantidade
 //tipo 1: compra (saldo inicial + quantidade)
 //tipo 2: venda (saldo inicial - quantidade)...
-    let tipoEntrada1 = prompt('Qual o tipo de entrada?')
-    let resultado1 = "Tipo de entrada:" + " " + tipoEntrada1;
-    alert(resultado1);
 //Definir as possíveis opções de tipos de entrada!
-    if (tipoEntrada1 = "compra") {
-        let compra = prompt('Qual a quantidade?')
-        let result1 = "Quantidade:" + " " + compra;
+    let tipoEntrada1 = prompt('Qual o tipo de entrada?')
+        tipoEntrada1 = 'compra','venda'
+//Definir a quantidade das entradas!
+    let tipoQuantidade = prompt('Qual a quantidade?')
+        result1 = "Quantidade:" + " " + tipoQuantidade;
         console.log(result1);
-    }
-    else if(tipoEntrada1= "venda") {
-        let venda = prompt('Qual a quantidade?')
-        let result2 = "Quantidade:" + " " + venda;
-        console.log(result2);
-    };
 //Definir os calculos de cada tipo de entrada
-if (venda < saldoInicial) {
-    let venda = venda;
-    let saldoInicial = saldoInicial;
-}
+    let compra = tipoQuantidade
+    while(compra + saldoInicial) {
+        alertaCompra = "Total atualizado:" + " "
+        console.log(alertaCompra,(parseInt(tipoQuantidade) + parseInt(saldoInicial)))
+    };
+    let venda = tipoQuantidade
+    if (saldoInicial <= venda){
+        alertaVenda = "Total atualizado:" + " "
+        console.log(alertaVenda,(parseInt(tipoQuantidade) - parseInt(saldoInicial)))
+    }
+    else if (venda > saldoInicial) {
+        alert('Saldo Insuficiente!')
+    };
